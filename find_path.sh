09b756743d69bd8d1969e26a837159b1e5d6454d
Konrad <konrad.handrick@axelera.ai>
2024-08-06 16:39:35 +0200

    update script

--- find_path.sh
+++ find_path.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-grep -rn "search_string" path/ | awk -F: '{print $1 ":" $2}' | xargs -I {} echo "break {}" > gdb_breaks.txt
+echo "set breakpoint pending on" > gdb_breaks.txt
+grep -rn "search_string" path/ | awk -F: '{print $1 ":" $2}' | xargs -I {} echo "break {}" >> gdb_breaks.txt
+echo "run" >> gdb_breaks.txt
 gdb -x gdb_breaks.txt -args my_program
+
